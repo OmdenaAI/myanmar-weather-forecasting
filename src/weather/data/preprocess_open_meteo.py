2ae6bc8be0f407a83f409072b04172d6f138ff9e
Carlos <carlosgdcj@hotmail.com>
2023-06-30 09:55:51 -0300

    Initial version of the preprocessing script

--- src/weather/data/preprocess_open_meteo.py
+++ src/weather/data/preprocess_open_meteo.py
@@ -1,5 +1,29 @@
+import pathlib
+import json
+import pandas as pd
+import missingno
+
+
 def main():
-    pass
+    data_path = pathlib.Path(__file__).parent.parent.parent.parent / "data" / "weather"
+    input_file = data_path / "open_meteo.json"
+
+    if not input_file.exists():
+        raise FileNotFoundError(
+            f'File "{input_file}" not found. Did you run the open_meteo.py scritp?'
+        )
+
+    with input_file.open("r") as f:
+        data = json.load(f)
+
+    df = pd.DataFrame(data["daily"])
+    df["latitude"] = data["latitude"]
+    df["longitude"] = data["longitude"]
+    df["elevation"] = data["elevation"]
+    df["country"] = "Myanmar"
+    df["city"] = "Tha Phay Wa"
+
+    df.to_csv(data_path / "open_meteo.csv", index=False)
 
 
 if __name__ == "__main__":
