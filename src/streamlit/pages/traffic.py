f3e09f04a9551cf30a920cf5e973031e1096f033
CarlosGDCJ <32521936+CarlosGDCJ@users.noreply.github.com>
2023-07-28 11:39:19 -0300

    Added text to traffic page

--- src/streamlit/pages/traffic.py
+++ src/streamlit/pages/traffic.py
@@ -7,10 +7,48 @@ st.set_page_config(
     page_icon="src/streamlit/img/favicon.png",
 )
 
+# styles
+# Streamlit doesn't allow to change the color of the progress bar, so we have to do with CSS
+# The idea was to have a blue-red gradient, where the bar was blue if traffic was low and turned red until Traffic Jam
+st.markdown(
+    """
+<style>
+
+    .stProgress {
+        transform: scale(-1, 1);
+    }
+    .stProgress > div > div > div > div {
+        background-color: #f0f2f6;
+        border-radius: 0;
+    }
+    .stProgress > div > div > div {
+        height: 15px;
+        background-image: linear-gradient(0.25turn, red, blue);
+        border-radius: 0;
+    }
+</style>
+""",
+    unsafe_allow_html=True,
+)
+
 st.header("Traffic Congestion")
+st.markdown(
+    "We dediced to frame the traffic congestion problem as a computer vision problem."
+    " The idea was to work with images of traffic cameras and classify the current"
+    ' traffic situation the camera was "seeing". \n\nThe source of data was Kaggle\'s'
+    " [Traffic Density"
+    " Singapore](https://www.kaggle.com/datasets/rahat52/traffic-density-singapore)"
+    " dataset. It contains images from Singapore's [Land Transport"
+    " Authority](https://www.lta.gov.sg/) labeled as 5 classes: **Empty**, **Low**,"
+    " **Medium**, **High** and **Traffic Jam**"
+)
 
 model_map = {"EfficientNet": ENb0, "ResNet": ResNet18}
 st.subheader("Choose a model")
+st.markdown(
+    "Due to the limited resources of Streamlit Could, we had to work with models that"
+    " were small in size."
+)
 option = st.selectbox(
     label="Choose a model",
     options=["EfficientNet", "ResNet"],
@@ -18,14 +56,7 @@ option = st.selectbox(
 )
 st.divider()
 
-st.subheader("Upload an image of traffic to classify")
-uploaded_file = st.file_uploader(
-    label="Upload an image of traffic to get a classification",
-    accept_multiple_files=False,
-    type=["png", "jpg"],
-    label_visibility="collapsed",
-)
-st.subheader("Or select one of the template images")
+st.subheader("Select one of the example images")
 sample_img = image_select(
     label="",
     images=[
@@ -39,7 +70,22 @@ sample_img = image_select(
     ],
     use_container_width=True,
 )
-traffic_map = {"Empty": 0, "Low": 25, "Medium": 50, "High": 75, "Traffic Jam": 100}
+
+st.subheader("or upload one")
+st.markdown(
+    "Uploaded images take precedence over the example ones. You can grab a real time"
+    " image from Singapore's traffic cameras"
+    " [here](https://onemotoring.lta.gov.sg/content/onemotoring/home/driving/traffic_information/traffic-cameras.html),"
+    " but feel free to test the model any other traffic image"
+)
+uploaded_file = st.file_uploader(
+    label="Uploaded images take precedence over the example images",
+    accept_multiple_files=False,
+    type=["png", "jpg"],
+    label_visibility="collapsed",
+)
+# Values are inverted because of CSS. See #styles above
+traffic_map = {"Empty": 100, "Low": 75, "Medium": 50, "High": 25, "Traffic Jam": 0}
 predicted_class = None
 if uploaded_file is not None:
     st.divider()
@@ -61,7 +107,7 @@ elif sample_img:
     predicted_class = model.inference(sample_img)
 
 if predicted_class is not None:
-    st.write(f"Predicted traffic class: {predicted_class}")
+    st.markdown(f"Traffic class: **{predicted_class}**")
     st.progress(
         traffic_map[predicted_class],
     )
